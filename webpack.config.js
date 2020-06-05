98edf772f155e2acb85d68745a14075baee5741c
manojsingh047 <manojsingh047@gmail.com>
2020-06-05 23:21:13 +0530

    parts strategy, using webpack merge

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,31 @@
+const merge = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-    plugins: [
-        //to automatically have the html file 
-        new HtmlWebpackPlugin({
-            title: "Webpack demo",
-        }),
-    ],
-    devServer: {
-        // Display only errors to reduce the amount of output.
-        stats: "errors-only",
+const parts = require("./webpack.parts");
 
-        // Parse host and port from env to allow customization.
-        //
-        // If you use Docker, Vagrant or Cloud9, set
-        // host: "0.0.0.0";
-        //
-        // 0.0.0.0 is available to all network devices
-        // unlike default `localhost`.
-        host: process.env.HOST, // Defaults to `localhost`
-        port: process.env.PORT, // Defaults to 8080
-        open: true, // Open the page in browser
-        overlay: true,
+const commonConfig = merge([
+    {
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: "Webpack demo",
+            }),
+        ],
     },
+]);
+const productionConfig = merge([]);
+
+const developmentConfig = merge([
+    parts.devServer({
+        // Customize host/port here if needed
+        host: process.env.HOST,
+        port: process.env.PORT,
+    }),
+]);
+
+module.exports = mode => {
+    if (mode === "production") {
+        return merge(commonConfig, productionConfig, { mode });
+    }
+
+    return merge(commonConfig, developmentConfig, { mode });
 };
